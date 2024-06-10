2695b8ef0fd82211c05d64a09c2d4f9045acd54c
Akash Kumar <65179310+AKAS1012@users.noreply.github.com>
2024-06-10 18:30:56 +0530

    Delete backend directory

--- backend/resume/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import * 
-
-
-class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = UserProfileImage
-        fields = ['id','image', 'username', 'userProfile']
--- backend/resume/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserProfileList
-
-
-urlpatterns = [
-    path('UserPro/', UserProfileList.as_view())
-
-]
